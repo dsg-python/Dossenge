56beec14545efaa34d0d0e27c429bf13e1d5d47c
Dossenge <files@anuonetwork.top>
2025-12-26 19:26:56 +0800

    Dossenge 1.0.post1

--- setup.py
+++ setup.py
@@ -12,8 +12,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='dossenge',
-    version='1.0',
-    description='Dossenge 1.0',
+    version='1.0.post1',
+    description='Dossenge 1.0.post1 紧急修复',
     long_description=open('README.md','r',encoding='utf-8').read(),
     long_description_content_type='text/markdown',
     author='Dossenge',
